379d22049320356d0ec46cecd847417fa16f38f2
Gessielle <gessielle-barossi@hotmail.com>
2020-09-12 15:20:13 -0300

    Cidades do Litoral sul

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;      
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cidades do Litoral Sul:");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Pedro de Toledo");
+            Console.WriteLine("Miracatu");
+            Console.WriteLine("Eldorado");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Santos");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Iguape");
+        }
+    }
+}
